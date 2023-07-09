37634d74f1a90e0d6a0e9cadb76efb3532b32137
Cory Fabian <cory fabian coryf65@users.noreply.github.com>
2023-07-08 19:29:31 -0500

    can now control player truck

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float _speed = 5.0f;
+    [SerializeField] private float _turnSpeed = 2.0f;
+    [SerializeField] private float _horizontalInput;
+    [SerializeField] private float _forwardInput;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * _speed);
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _forwardInput = Input.GetAxis("Vertical");
+        
+        transform.Translate(Vector3.forward * Time.deltaTime * _speed * _forwardInput);
+        transform.Translate(Vector3.right * Time.deltaTime * _turnSpeed * _horizontalInput);
     }
 }
