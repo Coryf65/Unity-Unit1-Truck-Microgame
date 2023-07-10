f9d4a84e8aae32f7178e9909570011cb4ec01960
Cory Fabian <cory fabian coryf65@users.noreply.github.com>
2023-07-09 23:59:00 -0500

    created a camera switcher from 1st person to 3rd person

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,15 +3,43 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 { 
-    [SerializeField] private Camera _mainCamera = null;
+    [SerializeField] private Camera _firstPersonCamera = null;
+    [SerializeField] private Camera _thirdPersonCamera = null;
     [SerializeField] private GameObject _objectToFollow = null;
 
     [Tooltip("Offset Camera view from player vehicle")]
     public Vector3 cameraOffset = new Vector3(0, 0, 0);
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            // which camera is enabled 
+            if (_firstPersonCamera.enabled)
+            {
+                ToggleCamera(_thirdPersonCamera, _firstPersonCamera);
+            }
+            else
+            {
+                ToggleCamera(_firstPersonCamera, _thirdPersonCamera);
+            }
+        }
+    }
+
     private void LateUpdate()
     {
         // offset camera to see better
         transform.position = _objectToFollow.transform.position + cameraOffset;
+    }
+
+    /// <summary>
+    /// Simple toggle for cameras between First and Third Person. Turns on one camera and off the other.
+    /// </summary>
+    /// <param name="viewToUse">Camera perspective to enable</param>
+    /// <param name="viewToHide">Camera perspective to hide</param>
+    private void ToggleCamera(Camera viewToUse, Camera viewToHide)
+    {
+        viewToUse.enabled = true;
+        viewToHide.enabled = false;
     }
 }
