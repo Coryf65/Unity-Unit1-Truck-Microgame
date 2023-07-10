a956332fb04536eaf4f810553dac3a54b4ec0d5e
Cory Fabian <cory fabian coryf65@users.noreply.github.com>
2023-07-10 18:56:11 -0500

    now using new input system and handling the player movement.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -11,60 +7,48 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _speed = 5.0f;
     [Range(0, 50)]
     [SerializeField] private float _turnSpeed = 2.0f;
-
     private PlayerInputActions playerInputActions;
     private InputAction movement;
     
-
     private void Awake()
     {
         playerInputActions = new();
     }
 
+    /// <summary>
+    /// Called when this GameObject is enabled
+    /// </summary>
     private void OnEnable()
     {
         movement = playerInputActions.Player1.Movement;
         movement.Enable();
-        
-        //playerInputActions.Player1.
     }
 
+    /// <summary>
+    /// Called when this GameObject is Disabled
+    /// </summary>
     private void OnDisable()
     {
         movement.Disable();
         playerInputActions.Player1.Movement.Disable();
     }
 
+    /// <summary>
+    /// Update Loop called every frame
+    /// </summary>
     private void Update()
     {
-        Debug.Log($"Movement Values: {movement.ReadValue<Vector2>()}");
+        HandleMovement(movement.ReadValue<Vector2>());
     }
 
-    /*Vector3 forwardInput = playerInputActions.Player1.Movement.ReadValue<Vector3>();
-
-       if (forwardInput.x > 0 || forwardInput.y > 0 || forwardInput.z > 0)
-       {
-           Debug.Log($"input: {forwardInput}");
-       
-           transform.Translate(forwardInput * Time.deltaTime * _speed);
-       }*/
-    
-    /*
-    public void HandleRotationMove(InputAction.CallbackContext context)
+    /// <summary>
+    /// Handle the player movement. forwards, backwards, rotating left and right.
+    /// Translates Y input into Z output, and X input into Rotational movement.
+    /// </summary>
+    /// <param name="inputMovement">Input Vector2 data, from the New Unity Input System.</param>
+    void HandleMovement(Vector2 inputMovement)
     {
-        float horizontalInput = 0f;
-        transform.Rotate(Vector3.up, _turnSpeed * horizontalInput * Time.deltaTime);
-    }*/
-
-    /*public void HandleForwardMove(InputAction.CallbackContext context)
-    {
-        float forwardInput = 0f;
-        Debug.Log(context);
-        
-        if (context.performed)
-        {
-            Debug.Log(context.ReadValue<float>());
-            transform.Translate(Vector3.forward * Time.deltaTime * _speed * forwardInput);
-        }
-    }*/
+        transform.Translate(0, 0, inputMovement.y * Time.deltaTime * _speed);
+        transform.Rotate(Vector3.up, _turnSpeed * inputMovement.x * Time.deltaTime);
+    }
 }
