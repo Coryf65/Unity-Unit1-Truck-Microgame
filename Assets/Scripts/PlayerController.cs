9c7d885b465c00d4f5678093980f0025a07094d7
Cory Fabian <cory fabian coryf65@users.noreply.github.com>
2023-07-10 12:04:57 -0500

    switching to newer unity input system for fun

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,22 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Range(0, 4)]
+    public int playerNumber = 0;
     [Range(0, 20)]
     [SerializeField] private float _speed = 5.0f;
     [Range(0, 50)]
     [SerializeField] private float _turnSpeed = 2.0f;
     private float horizontalInput;
     private float forwardInput;
-    
+
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
-        
+        horizontalInput = Input.GetAxis("Horizontal" + playerNumber);
+        forwardInput = Input.GetAxis("Vertical" + playerNumber);
+
         transform.Translate(Vector3.forward * Time.deltaTime * _speed * forwardInput);
         transform.Rotate(Vector3.up, _turnSpeed * horizontalInput * Time.deltaTime);
     }
